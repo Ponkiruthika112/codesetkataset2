78989d4fca09d0065f6a24ee937daa22fe6cdb2b
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-01-08 16:14:23 +0530

    Update cod6.py

--- cod6.py
+++ cod6.py
@@ -1,9 +1,11 @@
-#this is my code
+#this is my code for prime
 n,k=map(int,input().split())
 for i in range(n+1,k):
 	for j in range(2,i):
 		if i%j==0:
 			break
 	if j==i-1:
-		print(i,end=" ")
+		s=s+str(i)+" "
+	
+print(s.strip())
 	
