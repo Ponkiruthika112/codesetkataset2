52a3fe74d563ffedf77d09eb3781b56712db7dc2
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-13 01:00:26 +0800

    Create rep_k.py

---
+++ rep_k.py
@@ -0,0 +1,9 @@
+n,k=map(int,input().split())
+a=list(map(int,input().split()))
+l=list(set(a))
+s=""
+for i in range(0,len(l)):
+    if a.count(l[i])==k:
+        s=s+str(a[i])+" "
+print(s.strip())
+#repeat k times
